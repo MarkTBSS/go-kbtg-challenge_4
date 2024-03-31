14867653f37123543c3441ff4d515a6c79cd0c50
MARK\pirat <marktbss@gmail.com>
2024-03-31 08:26:03 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/MarkTBSS/go-kbtg-challenge_4/configs"
+	"github.com/MarkTBSS/go-kbtg-challenge_4/postgres"
+	"github.com/MarkTBSS/go-kbtg-challenge_4/wallet"
+	"github.com/labstack/echo/v4"
+
+	_ "github.com/MarkTBSS/go-kbtg-challenge_4/docs"
+	echoSwagger "github.com/swaggo/echo-swagger"
+)
+
+// @title			Wallet API
+// @version		1.0
+// @description	Sophisticated Wallet API
+// @host			localhost:1323
+func main() {
+	configs.LoadConfig()
+	databaseInstance, err := postgres.New()
+	if err != nil {
+		panic(err)
+	}
+	echoInstance := echo.New()
+	echoInstance.GET("/swagger/*", echoSwagger.WrapHandler)
+	handler := wallet.New(databaseInstance)
+	echoInstance.GET("/api/v1/wallets", handler.WalletHandler)
+	echoInstance.GET("/api/v1/wallets/query", handler.QueryParamHandler)
+	echoInstance.GET("/api/v1/users/:id/wallets", handler.PathParamHandler)
+	echoInstance.Logger.Fatal(echoInstance.Start(":1323"))
+}
---
+++ postgres/wallet.go
@@ -0,0 +1,111 @@
+package postgres
+
+import (
+	"time"
+
+	"github.com/MarkTBSS/go-kbtg-challenge_4/wallet"
+)
+
+type Wallet struct {
+	ID         int       `postgres:"id"`
+	UserID     int       `postgres:"user_id"`
+	UserName   string    `postgres:"user_name"`
+	WalletName string    `postgres:"wallet_name"`
+	WalletType string    `postgres:"wallet_type"`
+	Balance    float64   `postgres:"balance"`
+	CreatedAt  time.Time `postgres:"created_at"`
+}
+
+func (postgres *Postgres) Wallets() ([]wallet.Wallet, error) {
+	rows, err := postgres.Database.Query("SELECT * FROM user_wallet")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var wallets []wallet.Wallet
+	for rows.Next() {
+		var w Wallet
+		err := rows.Scan(&w.ID,
+			&w.UserID, &w.UserName,
+			&w.WalletName, &w.WalletType,
+			&w.Balance, &w.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		wallets = append(wallets, wallet.Wallet{
+			ID:         w.ID,
+			UserID:     w.UserID,
+			UserName:   w.UserName,
+			WalletName: w.WalletName,
+			WalletType: w.WalletType,
+			Balance:    w.Balance,
+			CreatedAt:  w.CreatedAt,
+		})
+	}
+	return wallets, nil
+}
+
+func (postgres *Postgres) GetByType(walletType string) ([]wallet.Wallet, error) {
+	query := "SELECT * FROM user_wallet WHERE wallet_type = $1"
+	rows, err := postgres.Database.Query(query, walletType)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var wallets []wallet.Wallet
+	for rows.Next() {
+		var w Wallet
+		err := rows.Scan(&w.ID,
+			&w.UserID, &w.UserName,
+			&w.WalletName, &w.WalletType,
+			&w.Balance, &w.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		wallets = append(wallets, wallet.Wallet{
+			ID:         w.ID,
+			UserID:     w.UserID,
+			UserName:   w.UserName,
+			WalletName: w.WalletName,
+			WalletType: w.WalletType,
+			Balance:    w.Balance,
+			CreatedAt:  w.CreatedAt,
+		})
+	}
+	return wallets, nil
+}
+func (postgres *Postgres) GetByUserID(id string) ([]wallet.Wallet, error) {
+	query := "SELECT * FROM user_wallet WHERE user_id = $1"
+	rows, err := postgres.Database.Query(query, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var wallets []wallet.Wallet
+	for rows.Next() {
+		var w Wallet
+		err := rows.Scan(&w.ID,
+			&w.UserID, &w.UserName,
+			&w.WalletName, &w.WalletType,
+			&w.Balance, &w.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+		wallets = append(wallets, wallet.Wallet{
+			ID:         w.ID,
+			UserID:     w.UserID,
+			UserName:   w.UserName,
+			WalletName: w.WalletName,
+			WalletType: w.WalletType,
+			Balance:    w.Balance,
+			CreatedAt:  w.CreatedAt,
+		})
+	}
+	return wallets, nil
+}
